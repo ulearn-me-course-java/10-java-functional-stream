8e5fbbd4d1674320eec5cff1cd0b69aeb27b560f
Henkka <henkkawar@gmail.com>
2019-05-08 23:31:44 +0500

    first

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,26 @@
 package com.example.task02;
 
+import java.math.BigInteger;
 import java.util.stream.IntStream;
 
 public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
-        cycleGrayCode(2)
-                .limit(10)
-                .forEach(System.out::println);
-        */
+
 
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n < 1 || n > 16)
+            throw new IllegalArgumentException();
+        int countInCycle = (int) Math.pow(2, n);
+        return IntStream.iterate(0, i -> i + 1)
+                .map(i -> g(i % countInCycle));
+    }
 
-        return null; // your implementation here
-
+    private static int g (int n) {
+        return n ^ (n >> 1);
     }
 
 }
