dc8cd76cd7fb6b360d3790b3393e859ff367b06b
shee4 <lilite71@gmail.com>
2019-05-10 06:54:42 +0500

    tasking

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,29 +1,26 @@
 package com.example.task01;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-
-        // TODO С корректно реализованным методом ternaryOperator должен компилироваться и успешно работать следующий код:
-
-        /*
         Predicate<Object> condition = Objects::isNull;
         Function<Object, Integer> ifTrue = obj -> 0;
         Function<CharSequence, Integer> ifFalse = CharSequence::length;
         Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
-        */
+        System.out.println(safeStringLength.apply("hell"));
 
     }
 
-    public static <T, U> Function<T, U> ternaryOperator(
-            Predicate<? super T> condition,
-            Function<? super T, ? extends U> ifTrue,
-            Function<? super T, ? extends U> ifFalse) {
+    public static <T, U> Function<T, U> ternaryOperator(Predicate<? super T> condition,
+                                                        Function<? super T, ? extends U> ifTrue,
+                                                        Function<? super T, ? extends U> ifFalse) {
 
-        return null; // your implementation here
+        if(condition == null || ifFalse == null || ifTrue == null) throw new NullPointerException();
+        return r -> condition.test(r) ? ifTrue.apply(r) : ifFalse.apply(r);
 
     }
 }
