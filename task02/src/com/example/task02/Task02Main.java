138a7155b678d09b9fc9cb6a3bdec129dc3d3886
Platonov2 <jek1999060@mail.ru>
2019-05-15 16:14:01 +0500

    Платонов Евгений. Задание 02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,38 @@
 package com.example.task02;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
-        cycleGrayCode(2)
+
+        cycleGrayCode(3)
                 .limit(10)
                 .forEach(System.out::println);
-        */
+
 
     }
 
     public static IntStream cycleGrayCode(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
 
-        return null; // your implementation here
+        for (int i = 1; i < n; i++) {
+            ArrayList<Integer> reverse = new ArrayList<>(list);
+            Collections.reverse(reverse);
 
+            for (int j = 0; j < reverse.size(); j++) {
+                reverse.set(j, reverse.get(j) + (int)Math.pow(2,i));
+            }
+            list.addAll(reverse);
+        }
+        return list.stream().mapToInt(x -> x);
     }
-
 }
