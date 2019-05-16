0ac6b621c779c6ccac2c0106a2e892b377498047
student <jek1999060@mail.ru>
2019-05-16 13:03:37 +0500

    asdfghj

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,9 +1,6 @@
 package com.example.task02;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -12,7 +9,7 @@ public class Task02Main {
     public static void main(String[] args) {
 
 
-        cycleGrayCode(3)
+        cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
 
@@ -20,19 +17,24 @@ public class Task02Main {
     }
 
     public static IntStream cycleGrayCode(int n) {
-        ArrayList<Integer> list = new ArrayList<>();
+        List<Integer> list = new LinkedList<>();
         list.add(0);
         list.add(1);
 
+        if (n > 16 || n == 0){
+            throw new IllegalArgumentException();
+        }
         for (int i = 1; i < n; i++) {
-            ArrayList<Integer> reverse = new ArrayList<>(list);
+            List<Integer> reverse = new LinkedList<>(list);
             Collections.reverse(reverse);
 
             for (int j = 0; j < reverse.size(); j++) {
-                reverse.set(j, reverse.get(j) + (int)Math.pow(2,i));
+                reverse.set(j, reverse.get(j) + (1 << i));
             }
             list.addAll(reverse);
         }
-        return list.stream().mapToInt(x -> x);
+        Integer[] arr = new Integer[list.size()];
+        list.toArray(arr);
+        return Stream.iterate(0, i -> i + 1).mapToInt(i -> list.get(i % list.size()));
     }
 }
