dab42301010e5d714654cab5004be49039880d4c
Max <uue33@mail.ru>
2019-05-20 14:03:13 +0500

    Задание 03. Иконниковf

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,15 @@
 package com.example.task03;
 
+import org.assertj.core.util.Streams;
+
+import java.awt.*;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.BiConsumer;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Task03Main {
@@ -21,7 +29,11 @@ public class Task03Main {
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-
-        // your implementation here
+        List<T> list = stream.sorted(order).collect(Collectors.toList());
+        if (list.isEmpty()) minMaxConsumer.accept(null, null);
+        else
+            minMaxConsumer.accept(list.get(0),
+                    list.get(list.size() - 1));
     }
 }
+
