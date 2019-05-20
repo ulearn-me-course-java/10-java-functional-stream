a6a4b91a34c4e51e1923b272c667b40168155019
uytera <uytera12@yandex.ru>
2019-05-20 17:34:01 +0500

    Даниил Бакулин. Задание03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -21,7 +21,15 @@ public class Task03Main {
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-
+            T[] arr = (T[]) stream.sorted(order).toArray();
+            if(arr.length == 0)
+            {
+                minMaxConsumer.accept(null, null);
+                return;
+            }
+            T min = arr[0];
+            T max = arr[arr.length-1];
+            minMaxConsumer.accept(min, max);
         // your implementation here
     }
 }
