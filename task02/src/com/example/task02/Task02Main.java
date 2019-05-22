50ba3db714a861a5eb4b9a0d823b13bb36fd3488
Vitaliy Smichkovskiy <32594950+ProgressiveArt@users.noreply.github.com>
2019-05-22 20:35:27 +0300

    Задание 02 Смычковский В.
    # Задание 02 Gray Code
    
    Напишите метод, возвращающий стрим чисел, соответствующих n-битным кодам Грея.
    
    Описание кода Грея: https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D0%B4_%D0%93%D1%80%D0%B5%D1%8F
    
    Битность кодов задается аргументом n в пределах от 1 до 16.
    
    Стрим должен выдавать коды циклически, то есть после конечного числа выдавать первое и так далее.
    
    #### Пример
    
    cycleGrayCode(2) должен вернуть стрим, состоящий из следующих чисел:
    
    0, 1, 3, 2, 0, 1, 3, 2, ... (дальше бесконечное количество повторений)
    
    cycleGrayCode(3) должен вернуть стрим, состоящий из следующих чисел:
    
    0, 1, 3, 2, 6, 7, 5, 4, 0 ... (и т.д.)

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,28 @@
 package com.example.task02;
 
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
     }
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
+        if (n < 1 || n > 16) throw new IllegalArgumentException();
 
+        Function<Integer, Integer> grayenCode = g -> g ^ (g >> 1);
+        int countInCircle = (int) Math.pow(2, n);
+
+        return IntStream
+                .iterate(0, i -> i + 1)
+                .map(i -> grayenCode.apply(i % (countInCircle)));
     }
-
 }
+
