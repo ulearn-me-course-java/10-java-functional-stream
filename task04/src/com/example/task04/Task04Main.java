60887802e1d3b3d1c65562d29bf80900a6d6e896
Mystache <Mystache@yandex.ru>
2019-05-23 12:04:23 +0500

    Solution #4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,47 @@
 package com.example.task04;
 
+import java.io.*;
+import java.util.*;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
+//        String test = "Е Е А А Г Г В В Б Б";
+//        System.setIn(new ByteArrayInputStream(test.getBytes()));
 
+        Map<String, Long> map = new TreeMap<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, UTF_8))) {
+                reader
+                    .lines()
+                    .map(String::toLowerCase)
+                    .filter(line -> !line.isEmpty())
+                    .forEach(
+                        line -> {
+                            String new_line = line.replaceAll("\\p{Punct}", " ").
+                                    replaceAll("  ", " ").replaceAll(" ", ",");
+
+                            for (String str: new_line.split(",")) {
+                                if (!map.containsKey(str))
+                                    map.put(str, 0L);
+
+                                Long counter = map.get(str);
+                                map.put(str, ++counter);
+                            }
+                        }
+                    );
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        map.entrySet().stream()
+            .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+            .limit(10)
+            .forEach(
+                v ->  System.out.print(v.getKey() + "\n")
+            );
     }
-
 }
