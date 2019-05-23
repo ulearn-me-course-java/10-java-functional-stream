c475cbe407f83de13082e09977af611b728a0e51
AllaOsipyan <allosipyan@mail.ru>
2019-05-23 20:02:02 +0500

    Осипян Пайцар. Задание02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,17 +6,18 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
+
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
+
 
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
+        if(n < 1 || n > 16) throw new IllegalArgumentException();
+        int combinations = (int) Math.pow(2,n);
+        return IntStream.iterate(0,i -> i + 1).map(i -> i%combinations).map(i -> i^(i >> 1)); // your implementation here
 
     }
 
