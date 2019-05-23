9c903e787a03fa8c961dc4085d9744861fd20035
uytera <uytera12@yandex.ru>
2019-05-23 17:18:49 +0500

    Даниил Бакулин. Задание04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,28 @@
 package com.example.task04;
+import java.io.IOException;
+import java.util.*;
 
-public class Task04Main {
 
-    public static void main(String[] args) {
+public class Task04Main<extend> {
 
-        // your implementation here
+    public static void main(String[] args) throws IOException {
+        Map<String, ComInt> map = new TreeMap<>();
 
-    }
-
+        try(Scanner bf = new Scanner(System.in)) {
+            String comp;
+            bf.useDelimiter("[-, ,!]");
+            while(bf.hasNext())
+            {
+               String temp = bf.next().toLowerCase();
+               if(map.containsKey(temp)) {
+                   map.get(temp).set(map.get(temp).intValue() + 1);
+               }
+               else map.put(temp, new ComInt(1));
+            }
+        }
+        map.entrySet().stream()
+                .sorted(Map.Entry.<String, ComInt>comparingByValue().reversed())
+                .limit(10)
+                .forEachOrdered(x-> System.out.println(x.getKey()));
+        }
 }
