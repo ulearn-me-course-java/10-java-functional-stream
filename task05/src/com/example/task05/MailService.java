ea57a0e7f36c43a68c8e0e747f26bba67176379a
Andrey Rakcheev <andre-orsk@yandex.ru>
2019-05-23 19:13:47 +0500

    Create MailService.java

---
+++ task05/src/com/example/task05/MailService.java
@@ -0,0 +1,23 @@
+package com.example.task05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class MailService<T> implements Consumer<IMessage<T>> {
+
+    private Map<String, List<T>> _mailBox = new HashMap<>();
+
+    public Map<String, List<T>> getMailBox() { return _mailBox; }
+
+    @Override
+    public void accept(IMessage<T> message) {
+
+        if(!_mailBox.containsKey(message.getTo()))
+            _mailBox.put(message.getTo(), new ArrayList<>());
+
+        _mailBox.get(message.getTo()).add(message.getContent());
+    }
+}
