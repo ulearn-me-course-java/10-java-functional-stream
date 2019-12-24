24b214b8e617418002dda121aac8e6ddf7b38f39
MintAnnetta <mintannetta@gmail.com>
2019-12-24 21:18:41 +0500

    Решена задача 10.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,33 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Comparator;
+import java.util.Map.Entry;
+import java.util.function.ToLongFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+// your implementation here
 public class Task04Main {
 
-    public static void main(String[] args) {
-
-        // your implementation here
-
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, Charset.defaultCharset()))) {
+            reader.lines()
+                    .map(String::toLowerCase)
+                    .flatMap(e -> Stream.of(e.split("\\P{L}+")))
+                    .filter(e -> !e.isEmpty())
+                    .collect(Collectors.groupingBy(n -> n, Collectors.counting()))
+                    .entrySet()
+                    .stream()
+                    .sorted(Comparator.comparingLong((ToLongFunction<Entry<String, Long>>) Entry::getValue)
+                            .reversed()
+                            .thenComparing(Entry::getKey))
+                    .limit(10)
+                    .forEach(e -> System.out.print(e.getKey() + "\n"));
+        }
     }
 
 }
