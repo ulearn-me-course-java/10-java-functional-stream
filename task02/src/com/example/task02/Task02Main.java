3e3536ea9344e818ff20909e0df7b91ba28f57c9
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-12-25 14:53:22 +0500

    [Матвеева] Решение задачи 10.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -15,9 +15,15 @@ public class Task02Main {
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if  ((n < 1) || (n > 16))
+            throw new IllegalArgumentException();
 
-        return null; // your implementation here
 
+        return IntStream.iterate(0, i -> i+1).map(i -> graycode(i%(int)Math.pow(2, n)));
+    }
+
+    private static int graycode(int n) {
+        return n^(n >> 1);
     }
 
 }
