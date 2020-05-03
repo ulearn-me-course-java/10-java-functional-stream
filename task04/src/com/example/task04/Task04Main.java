13b6b170589e2d578d37bdf6cc9283e9cd6622ac
LordOfTheDeadlines <vedroid28@mail.ru>
2020-05-03 14:19:00 +0500

    Сидорчук Анастасия. 10.03

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,30 @@
 package com.example.task04;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
+        Scanner scanner = new Scanner(System.in, "UTF-8")
+                .useDelimiter("[^\\p{L}\\p{Digit}]+");
 
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        scanner.forEachRemaining(s -> hashMap.merge(s.toLowerCase(), 1, Integer::sum));
+        hashMap.entrySet().stream()
+                .sorted(descendingFrequencyOrder())
+                .limit(Math.min(hashMap.size(), 10))
+                .map(Map.Entry::getKey)
+                .forEach(x->System.out.print(x+'\n'));
     }
 
+    private static Comparator<Map.Entry<String, Integer>> descendingFrequencyOrder() {
+        return Comparator.<Map.Entry<String, Integer>>comparingInt(Map.Entry::getValue)
+                .reversed()
+                .thenComparing(Map.Entry::getKey);
+    }
 }
+
