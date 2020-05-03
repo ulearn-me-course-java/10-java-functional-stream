cd21e2499c1baa35524cccca68500b1111d942d1
Наби Гадимов <gadimovnabi@gmail.com>
2020-05-03 21:57:52 +0500

    Гадимов Наби. Задание 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -5,19 +5,25 @@ import java.util.stream.IntStream;
 public class Task02Main {
 
     public static void main(String[] args) {
-
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n>17 || n<1){
+            throw new IllegalArgumentException("Argument is out of range");
+        }
 
-        return null; // your implementation here
+        return IntStream
+                .iterate(0, x->x+1)
+                .map(x->grayencode(x % (int)Math.pow(2,n)));
 
+    }
+    static int grayencode(int g)
+    {
+        return g ^ (g >> 1); //g XOR g(на 1 вправо)
     }
 
 }
