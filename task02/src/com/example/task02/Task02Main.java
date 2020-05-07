547188be834fd4d551a2f1395e5050ae042f15e2
yeroxo <yeroxo@gmail.com>
2020-05-07 23:04:43 +0500

    Колыванов Артем ПрИ 201 task 02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,18 +6,28 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
+
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
+
 
     }
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
+        if (n > 16 || n < 1)
+            throw new IllegalArgumentException();
 
+        int codeLength = (int) Math.pow(2, n);
+
+        return IntStream
+                .iterate(0, it -> (it + 1) % codeLength)
+                .map(Task02Main::nextGrayNumber);
+    }
+
+    private static int nextGrayNumber(int current){
+        return current ^ (current >> 1);
     }
 
 }
