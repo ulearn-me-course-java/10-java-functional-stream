33abe668227d26c6ba207f077bda560796bf6947
Id.Io.T <kdevyatkov-ilya@mail.ru>
2020-05-07 11:48:22 +0500

    upgrade task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,11 +2,13 @@ package com.example.task04;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.Map.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.function.ToLongFunction;
 import java.util.stream.Collectors;
@@ -17,22 +19,35 @@ import static java.util.stream.Collectors.groupingBy;
 
 public class Task04Main {
 
-        public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+        AtomicReference<StringBuilder> stringBuilder = new AtomicReference<>(new StringBuilder());
+        Map<String, Integer> stringMap = new HashMap<>();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in, Charset.defaultCharset());
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, Charset.forName("UTF-8")));
-            reader.lines()
-                    .map(String::toLowerCase)
-                    .flatMap(t -> Stream.of(t.split("\\P{L}+")))
-                    .filter(t -> !t.isEmpty())
-                    .collect(Collectors.groupingBy(n -> n, Collectors.counting()))
-                    .entrySet()
-                    .stream()
-                    .sorted(Comparator.comparingLong((ToLongFunction<Entry<String, Long>>) Entry::getValue)
-                            .reversed()
-                            .thenComparing(Entry::getKey))
-                    .limit(10)
-                    .forEach(t -> System.out.print(t.getKey() + "\n"));
+        Stream.iterate(inputStreamReader.read(),
+                n -> {
+                    char chr = (char) n.intValue();
+                    if (Character.isLetter(chr))
+                        stringBuilder.get().append(Character.toLowerCase(chr));
+                    else if (stringBuilder.get().length() != 0) {
+                        String word = stringBuilder.toString();
+                        stringMap.put(word, stringMap.containsKey(word) ? stringMap.get(word) + 1 : 1);
+                        stringBuilder.set(new StringBuilder());
+                    }
+                    try {
+                        return inputStreamReader.read();
+                    } catch (IOException e) {
+                        return -1;
+                    }
+                }).anyMatch(x -> x == -1);
 
+        stringMap.entrySet().stream()
+                .sorted((a, b) -> {
+                    Integer v1 = a.getValue();
+                    Integer v2 = b.getValue();
+                    return !v1.equals(v2) ? v2.compareTo(v1) : a.getKey().compareTo(b.getKey());
+                })
+                .limit(10)
+                .forEach(x -> System.out.print(x.getKey() + '\n'));
     }
-
 }
