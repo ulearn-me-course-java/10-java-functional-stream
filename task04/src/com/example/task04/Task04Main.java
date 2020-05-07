f27d8209248ee09be220f3b2f4f17cb5c3971630
Губайдуллина Виктория <vicmurrmiu@gmail.com>
2020-05-08 02:39:36 +0300

    Губайдуллина Виктория Задание10.04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,47 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
-
         // your implementation here
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        HashMap<String, Integer> dict = new HashMap<>();
+
+        in.lines()
+                .map(String::toLowerCase)
+                .flatMap(it -> Stream.of(it.split("\\P{LD}")))
+                .filter(it -> !it.isEmpty())
+                .forEach(it -> {
+                    if (!dict.containsKey(it))
+                        dict.put(it, 1);
+                    else
+                        dict.replace(it, dict.get(it) + 1);
+                });
+
+        dict.entrySet()
+                .stream()
+                .sorted(new EntryComparator())
+                .limit(10)
+                .forEach(it -> System.out.print(it.getKey() + "\n"));
 
     }
 
+}
+
+class EntryComparator implements Comparator<Map.Entry<String, Integer>> {
+
+    @Override
+    public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+        return e2.getValue().compareTo(e1.getValue()) != 0
+                ? e2.getValue().compareTo(e1.getValue())
+                : e1.getKey().compareTo(e2.getKey());
+    }
 }
