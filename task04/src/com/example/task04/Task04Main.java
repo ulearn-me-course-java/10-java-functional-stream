b3dd09ddcef7f074b7afdb149649d3c069dbf2e1
DarenOwl <DarenOwl@users.noreply.github.com>
2020-05-07 15:04:21 +0500

    Андреева Юлия. Задание 04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,27 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
-
+        new BufferedReader(new InputStreamReader(System.in))
+                .lines()
+                .flatMap(x -> Stream.of(x.split("[^\\p{L}]")))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .forEach(System.out::println);
     }
-
 }
