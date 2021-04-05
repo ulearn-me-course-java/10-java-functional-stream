4e020725c65e178ce4982c187fb48d35e4ede0b9
M <mihail.petuxov@yandex.ru>
2021-04-05 22:34:52 +0500

    Петухов Михаил. Задача 10.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,22 @@
 package com.example.task02;
-
 import java.util.stream.IntStream;
 
 public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
-        cycleGrayCode(2)
-                .limit(10)
-                .forEach(System.out::println);
-        */
+        cycleGrayCode(3).limit(10).forEach(System.out::println);
 
     }
 
-    public static IntStream cycleGrayCode(int n) {
+    public static IntStream cycleGrayCode(int n) throws IllegalArgumentException{
+        if (n <= 0 || n > 16)
+            throw new IllegalArgumentException();
+        int c = (int) Math.pow(2, n);
 
-        return null; // your implementation here
-
+        return IntStream.iterate(0, x -> x + 1)
+                .map(x -> x % c)
+                .map(x -> x ^ (x >> 1));
     }
 
 }
