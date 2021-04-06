c5f02d8063458996e591fc1160e7dfb25531b7f2
M <mihail.petuxov@yandex.ru>
2021-04-06 12:57:56 +0500

    Петухов Михаил. Задача 10.1

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,29 @@
 package com.example.task04;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
-
     public static void main(String[] args) {
-
-        // your implementation here
-
+        new BufferedReader(new InputStreamReader(System.in))
+                .lines()
+                .flatMap(x -> Stream.of((x.split("[\\P{L}&&\\P{Digit}]"))))
+                .filter(String::isEmpty)
+                .map(String::toLowerCase)
+                .collect(Collectors.toMap(k -> k, v -> 1, Integer::sum))
+                .entrySet()
+                .stream()
+                .sorted(Entry.comparingByKey())
+                .sorted(Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach(x -> System.out.print(x.getKey() + "\n"));
     }
-
 }
