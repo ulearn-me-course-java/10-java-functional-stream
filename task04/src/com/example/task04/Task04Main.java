eaf187914dcaae180a7a53af7d966275762e3287
M <mihail.petuxov@yandex.ru>
2021-04-06 13:03:30 +0500

    Петухов Михаил. Задача 10.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,16 +1,10 @@
 package com.example.task04;
-
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.Comparator;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 public class Task04Main {
     public static void main(String[] args) {
         new BufferedReader(new InputStreamReader(System.in))
