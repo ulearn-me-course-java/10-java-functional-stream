9a8fda1bdb3fd9b43639ed14f3a1705942922805
DarkLight <max.gubar@outlook.com>
2021-04-20 17:25:11 +0500

    Губарь Максим task4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,27 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
-
-        // your implementation here
-
+        new BufferedReader(new InputStreamReader(System.in)).
+                lines().
+                map(String::toLowerCase).
+                flatMap(x -> Stream.of(x.split("[\\P{L}&&\\P{Digit}]"))).
+                filter(x -> !x.isEmpty()).
+                collect(Collectors.toMap(key -> key, value -> 1, Integer::sum)).
+                entrySet().
+                stream().
+                sorted(Map.Entry.comparingByKey()).
+                sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).
+                limit(10).
+                forEach(x -> System.out.print(x.getKey() + "\n"));
     }
 
 }
