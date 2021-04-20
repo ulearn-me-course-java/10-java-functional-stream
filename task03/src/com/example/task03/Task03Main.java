18ccb9abb3f2c6068fac143ca2bf9f4f22d8bb97
Wither87 <wither_87@mail.ru>
2021-04-20 13:17:13 +0500

    Крутолевич Сергей. Задача 10.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,6 +1,7 @@
 package com.example.task03;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
@@ -21,7 +22,23 @@ public class Task03Main {
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
+        Iterator<? extends T> iterator = stream.iterator();
+        if (!iterator.hasNext()){
+            minMaxConsumer.accept(null, null);
+            return;
+        }
 
-        // your implementation here
+        T current = (T) iterator.next();
+        T min = current;
+        T max = current;
+
+        while (iterator.hasNext()){
+            current = (T) iterator.next();
+            if (order.compare(current, max) > 0)
+                max = current;
+            else if (order.compare(current, min) < 0)
+                min = current;
+        }
+        minMaxConsumer.accept(min, max);
     }
 }
