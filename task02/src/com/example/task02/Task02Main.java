f82e87a268df4990d411e223d1dcacf5f41d8ed7
DarkLight <max.gubar@outlook.com>
2021-04-20 16:32:17 +0500

    Губарь Максим task2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -15,8 +15,10 @@ public class Task02Main {
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
+        if (n < 1 || n > 16)
+            throw new IllegalArgumentException();
+        int size = (int) Math.pow(2, n);
+        return IntStream.iterate(0, x -> x + 1).map(x -> x % size).map(x -> x ^ (x >> 1));
 
     }
 
