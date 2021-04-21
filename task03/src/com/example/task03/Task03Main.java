417adfe25743043a3a363c8c8a809b2554c76fce
ebuche-murloc <immortalmagicdend@gmail.com>
2021-04-21 20:26:55 +0500

    Быков_10.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,12 @@
 package com.example.task03;
 
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Task03Main {
@@ -14,7 +19,6 @@ public class Task03Main {
                 (min, max) ->
                         System.out.println("min: " + min + " / max: " + max)
         );
-
     }
 
     public static <T> void findMinMax(
@@ -22,6 +26,21 @@ public class Task03Main {
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
 
-        // your implementation here
+        Object[] min = new Object[]{null};
+        Object[] max = new Object[]{null};
+        Iterator<? extends T> iter = stream.iterator();
+
+        if (iter.hasNext()) {
+            T val = iter.next();
+            min[0] = val;
+            max[0] = val;
+        }
+
+        iter.forEachRemaining(x -> {
+            if (order.compare(x, (T) min[0]) < 0) min[0] = x;
+            if (order.compare(x, (T) max[0]) > 0) max[0] = x;
+        });
+
+        minMaxConsumer.accept((T) min[0], (T) max[0]);
     }
 }
