09e33fc482841ecccb926e05847c355d5a68b398
Evgeniy <djonich@mail.ru>
2021-04-21 23:11:05 +0500

    Ничипоренко Евгений. Задание 10.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -15,9 +15,17 @@ public class Task02Main {
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n > 16 || n < 1) {
+            throw new IllegalArgumentException();
+        }
+        int count = (int) Math.pow(2, n);
+        return IntStream.iterate(0, x -> x + 1)
+                .map(x -> getGray(x % count));
 
-        return null; // your implementation here
+    }
 
+    public static int getGray(int g) {
+        return g ^ (g >> 1);
     }
 
 }
