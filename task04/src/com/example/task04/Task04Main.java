ed915efc09106f24522297421b2df1fe11d1f4af
Sofya <fiepaz03@gmail.com>
2021-04-21 20:39:41 +0500

    Task 10.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,10 +1,33 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            reader.lines()
+                    .flatMap(x -> Stream.of(x.split("[\\P{L}&&\\P{Digit}]")))
+                    .filter(x -> !x.isEmpty())
+                    .map(String::toLowerCase)
+                    .collect(Collectors.toMap(key -> key, value -> 1, Integer::sum))
+                    .entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .map(Map.Entry::getKey)
+                    .limit(10)
+                    .forEach(x->System.out.print(x + ' '));
+        }
+        catch (IOException exception) {
+            exception.printStackTrace();
+        }
 
     }
 
