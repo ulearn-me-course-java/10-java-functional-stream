922191b2e6d7c9465bfaa2fae4a8d5442b537240
Van Darkholme <jija@kaavo>
2021-04-22 15:57:15 +0500

    task05

---
+++ task05/src/com/example/task05/Salary.java
@@ -0,0 +1,8 @@
+package com.example.task05;
+
+public class Salary extends Message<Integer> {
+
+    public Salary(String company, String name, int salary) {
+        super(company, name, salary);
+    }
+}
