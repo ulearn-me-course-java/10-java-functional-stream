57e6fee1a6537c25e26f001b210e4f706c10962b
ebuche-murloc <immortalmagicdend@gmail.com>
2021-04-22 11:56:16 +0500

    Быков_10.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,10 +1,30 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.lines()
+                .flatMap(x -> Arrays
+                        .stream(x.toLowerCase(Locale.ROOT)
+                                .split("[^\\p{L}\\d]+")))
+                .filter(x -> !x.isEmpty())
+                .collect(Collectors.groupingBy(x -> x, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparingLong((Map.Entry<String, Long> x) -> x.getValue() * (long) (-1)).thenComparing(Map.Entry::getKey))
+                .limit(10)
+                .forEach(x -> System.out.print(x.getKey() + '\n'));
 
     }
 
