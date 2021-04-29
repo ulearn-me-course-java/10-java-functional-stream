7666f740947f71771c2bcf9bcd11fb749aad81b9
пк <nikitos.wie@mail.ru>
2021-04-29 15:59:31 +0500

    Яхин, Task - 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,17 +6,22 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
-        cycleGrayCode(2)
+
+        cycleGrayCode(3)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
+        int sum = IntStream.iterate(0,n -> n+1)
+                .limit(100)
+                .sum();
+        System.out.println(sum);
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
+        if (n < 1 || n > 16) { throw new IllegalArgumentException(); }
+        return IntStream.iterate(0,x -> x + 1)
+                .map(x -> (int)(x % Math.pow(2,n))) // Приведение к правильной форме осуществляется путём отбора по остатку.
+                .map(x -> x ^ (x >> 1));           // Формула из вики - взаимозаменяет парные разряды;
 
     }
 
