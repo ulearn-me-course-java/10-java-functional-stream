d40830c1cd87f55dc25ecc48d8ba24447a090a18
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2021-04-29 13:02:57 +0500

    Update Task02Main.java

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -5,19 +5,20 @@ import java.util.stream.IntStream;
 public class Task02Main {
 
     public static void main(String[] args) {
-
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
-
     }
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
+        if (n <= 0 || n > 16){
+            throw new IllegalArgumentException();
+        }
 
+        int mask = (1 << n) - 1;
+
+        return IntStream.iterate(0, x -> (x + 1) & mask).map(x -> x ^ (x >> 1));
     }
 
 }
