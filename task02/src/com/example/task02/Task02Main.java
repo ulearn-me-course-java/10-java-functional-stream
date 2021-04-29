071deed4c65170c56009a33534b6993d810ef44c
пк <nikitos.wie@mail.ru>
2021-04-29 16:00:33 +0500

    Яхин, Task - 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -5,8 +5,6 @@ import java.util.stream.IntStream;
 public class Task02Main {
 
     public static void main(String[] args) {
-
-
         cycleGrayCode(3)
                 .limit(10)
                 .forEach(System.out::println);
