4cc5a16b210e66c00622c766196e01e2d4ed8960
IlyaIIS <ilya1221221@mail.ru>
2022-04-25 18:04:24 +0300

    Исмаков Илья - task 3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,12 @@
 package com.example.task03;
 
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class Task03Main {
@@ -22,6 +27,22 @@ public class Task03Main {
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
 
-        // your implementation here
+        Iterator<? extends T> iterator = stream.iterator();
+        T min = null, max = null;
+        if (iterator.hasNext()) {
+            T firstElement = iterator.next();
+            min = firstElement;
+            max = firstElement;
+        }
+
+        while (iterator.hasNext()) {
+            T element = iterator.next();
+            if (order.compare(element, min) < 0)
+                min = element;
+            if (order.compare(element, max) > 0)
+                max = element;
+        }
+
+        minMaxConsumer.accept(min, max);
     }
 }
