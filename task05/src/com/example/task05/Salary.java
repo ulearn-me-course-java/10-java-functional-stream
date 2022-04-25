3dce14b1225afec701b15c9207daa790335f7b4a
Anny <70793585+anyast103@users.noreply.github.com>
2022-04-25 19:26:54 +0300

    Task_10.5

---
+++ task05/src/com/example/task05/Salary.java
@@ -0,0 +1,7 @@
+package com.example.task05;
+
+public class Salary extends AbstractMail<Integer>{
+    public Salary(String from, String to, Integer content) {
+        super(from, to, content);
+    }
+}
