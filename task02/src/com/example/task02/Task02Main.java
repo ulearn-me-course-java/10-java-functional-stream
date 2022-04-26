5713f26e1438af64241de9f655382318ccc7946d
eva <eva27122002b@gmail.com>
2022-04-27 01:05:58 +0500

    added task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,18 +6,18 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
-
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n <= 0 || n >= 17) throw new IllegalArgumentException(":c");
 
-        return null; // your implementation here
-
+        //int grayEncode = ;
+        return IntStream.iterate(0, x ->  x + 1)
+                .map(x -> x % (int) Math.pow(2, n))
+                .map(x -> x ^ (x >> 1)); // your implementation here
     }
 
 }
