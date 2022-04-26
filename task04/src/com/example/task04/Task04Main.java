72a05bf8be37d2dfa2a0fd8923056473bb1bfd27
IlyaIIS <ilya1221221@mail.ru>
2022-04-26 22:17:52 +0300

    Исмаков Илья - task 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,27 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Task04Main {
-
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        // your implementation here
-
+        Arrays.stream(reader.lines()
+                        .map(x -> x + " ")
+                        .flatMap(x -> x.chars().mapToObj(y -> (char) y))
+                        .map(x -> Character.isLetterOrDigit(x) ? x : ' ')
+                        .map(Character::toLowerCase)
+                        .map(String::valueOf).collect(Collectors.joining())
+                        .split(" ")
+                ).collect(Collectors.groupingBy(x -> x, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed()
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .limit(11)
+                .forEach(x -> System.out.print(x.getKey() + "\n"));
     }
-
 }
