54d49c3fa00e5873104391ecd6b4ba77e15fc8d5
Kseniya <barkas074@yandex.ru>
2022-04-27 00:13:22 +0500

    Revert "completed task03"
    This reverts commit 51611b51e7b28e28b419d602fdedbe1e60767b08.

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,6 @@
 package com.example.task03;
 
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
@@ -22,18 +21,7 @@ public class Task03Main {
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-        Iterator<? extends T> iterator = stream.iterator();
-        T current = iterator.hasNext() ? iterator.next() : null;
-        T min = current;
-        T max = current;
-        while (iterator.hasNext()) {
-            current = iterator.next();
-            if (order.compare(current, min) < 0) {
-                min = current;
-            } else if (order.compare(current, max) > 0) {
-                max = current;
-            }
-        }
-        minMaxConsumer.accept(min, max);
+
+        // your implementation here
     }
 }
