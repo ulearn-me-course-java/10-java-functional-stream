1263fd934850567a224d41f10d058144fc0e0124
Kseniya <barkas074@yandex.ru>
2022-04-26 23:56:59 +0500

    Revert "completed task01"
    This reverts commit 10ea64c0bf0b191705249c49f2f49fe494206af0.

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,16 +1,20 @@
 package com.example.task01;
 
 import java.io.IOException;
-import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
+
+        // TODO С корректно реализованным методом ternaryOperator должен компилироваться и успешно работать следующий код:
+
+        /*
         Predicate<Object> condition = Objects::isNull;
         Function<Object, Integer> ifTrue = obj -> 0;
         Function<CharSequence, Integer> ifFalse = CharSequence::length;
         Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+        */
 
     }
 
@@ -18,9 +22,8 @@ public class Task01Main {
             Predicate<? super T> condition,
             Function<? super T, ? extends U> ifTrue,
             Function<? super T, ? extends U> ifFalse) {
-        if (condition == null || ifFalse == null || ifTrue == null) {
-            throw new NullPointerException();
-        }
-        return (x) -> condition.test(x) ? ifTrue.apply(x) : ifFalse.apply(x);
+
+        return null; // your implementation here
+
     }
 }
