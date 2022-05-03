f4602d41185881d01ee6acd2b607e4508c26f5d7
sphinxan <dazaj.osamu@mail.ru>
2022-05-04 02:09:38 +0500

    Ковыляева Настя. Задание 10.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,18 +6,26 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
+
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
+
 
     }
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
+        if(n > 16 || n < 1) throw new IllegalArgumentException("значения от 1 до 16");
 
+        int grayCodeLength = (int) Math.pow(2, n);
+
+        return IntStream.iterate(0, i -> i + 1)
+                .map(i -> grayCode(i % grayCodeLength));
+    }
+
+    static int grayCode(int g){
+        return g ^ (g >> 1);
     }
 
 }
