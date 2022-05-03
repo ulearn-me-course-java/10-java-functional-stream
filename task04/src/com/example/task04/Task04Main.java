6a989815de401925da99f6115c8a26b992d624ad
ArsaGit <artyom.arsalanov@gmail.com>
2022-05-04 03:37:48 +0500

    Арсаланов Артём. Задание 10.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,30 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        // your implementation here
-
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            br.lines()
+                    .map(String::toLowerCase)
+                    .flatMap(str -> Stream.of(str.split("[^\\wа-яё]")))
+                    .filter(str -> !str.trim().isEmpty())
+                    .collect(Collectors.toMap(str -> str, value -> 1, Integer :: sum))
+                    . entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByKey())
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .limit(10)
+                    .forEach(str -> System.out.print(str.getKey() + "\n"));
+        }
     }
 
 }
