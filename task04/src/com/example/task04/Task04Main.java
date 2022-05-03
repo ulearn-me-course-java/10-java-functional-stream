252665b5a7dacf6c9d3eab78a4afc5cbc11079ff
Nanoster <danil2002.11@mail.ru>
2022-05-04 03:35:29 +0500

    Task 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,10 +1,37 @@
 package com.example.task04;
 
-public class Task04Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.ToLongFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-    public static void main(String[] args) {
+public class Task04Main
+{
 
-        // your implementation here
+    public static void main(String[] args)
+    {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)))
+        {
+            reader.lines()
+                    .flatMap(x -> Stream.of(x.toLowerCase().split("[^\\da-zA-Zа-яёА-ЯЁ]")))
+                    .filter(x -> !x.isEmpty())
+                    .collect(Collectors.groupingBy(x -> x, Collectors.counting()))
+                    .entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByKey())
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .limit(10)
+                    .forEach(x -> System.out.print(x.getKey() + '\n'));
+            ;
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
     }
 
