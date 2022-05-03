7be5c7e1b1bd7b122736b412e76fd0efba1f0ee7
ArsaGit <artyom.arsalanov@gmail.com>
2022-05-04 02:11:29 +0500

    Арсаланов Артём. Задание 10.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,18 +6,23 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
+
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
+        System.out.println(Integer.rotateLeft(1, 2));
+        System.out.println(1 << 2);
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
-
+        if(n < 1 || n > 16) throw new IllegalArgumentException();
+        int ceiling = 1 << n;
+        return IntStream.iterate(0, i -> i = i + 1).map(i -> {
+            i = i % ceiling;
+            i = i ^ (i >> 1);
+            return i;
+        });
     }
 
 }
