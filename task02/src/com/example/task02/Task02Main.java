3b4f6f3eefe41edb79e1fb38f211232f65290252
nodlya <nadya-popova-02@mail.ru>
2022-05-03 19:01:55 +0500

    Попова Надежда задание 10.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,5 +1,6 @@
 package com.example.task02;
 
+import java.io.IOException;
 import java.util.stream.IntStream;
 
 public class Task02Main {
@@ -15,9 +16,10 @@ public class Task02Main {
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
-
+        if (n > 16 || n < 1) throw new IllegalArgumentException("Чот не так");
+        return IntStream.iterate(0, x ->  x + 1)
+                .map(x -> x % (int) Math.pow(2, n))
+                .map(x -> x ^ (x >> 1));
     }
 
 }
