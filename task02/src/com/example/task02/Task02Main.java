22811fa39a91d6ce2f490381da56b76f62a9d246
gorosheek <yulyagroshenko@mail.ru>
2022-05-04 00:24:34 +0500

    Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,22 +2,29 @@ package com.example.task02;
 
 import java.util.stream.IntStream;
 
-public class Task02Main {
+public class Task02Main
+{
 
-    public static void main(String[] args) {
-
-        /*
+    public static void main(String[] args)
+    {
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
     }
 
-    public static IntStream cycleGrayCode(int n) {
+    public static IntStream cycleGrayCode(int n)
+    {
 
-        return null; // your implementation here
+        if (n > 0 && n < 17)
+        {
+            int length = (int) Math.pow(2, n);
+            return IntStream
+                    .iterate(0, i -> i + 1)
+                    .map(x -> (x % length) ^ ((x % length) >> 1));
 
+        }
+        throw new IllegalArgumentException();
     }
 
 }
