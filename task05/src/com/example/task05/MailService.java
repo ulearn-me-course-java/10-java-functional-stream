cd864ea8e08f559d8c0b5744512fcdd4cbe7eed0
Boris Tarelkin <tarelkinba@gmail.com>
2022-11-08 10:47:53 +0500

    all

---
+++ task05/src/com/example/task05/MailService.java
@@ -0,0 +1,34 @@
+package com.example.task05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class MailService<T> implements Consumer<BaseForm<T>> {
+    private final Map<String, List<T>> MailBox = new HashMap<String, List<T>>() {
+        @Override
+        public List<T> get(Object key) {
+            List<T> list = super.get(key);
+            return list != null ? list : new ArrayList<>();
+        }
+    };
+
+    public Map<String, List<T>> getMailBox() {
+
+        return MailBox;
+    }
+
+    @Override
+    public void accept(BaseForm<T> form) {
+        if (MailBox.containsKey(form.getTo())) {
+            MailBox.get(form.getTo()).add(form.getContent());
+        } else {
+            ArrayList<T> arrayList = new ArrayList<>();
+            arrayList.add(form.getContent());
+            MailBox.put(form.getTo(), arrayList);
+        }
+    }
+}
+
