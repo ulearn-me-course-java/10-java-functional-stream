1e58d5ab3cc68d86b7662aa4032c3f0706bcf908
ReedKat <goshaidze03@gmail.com>
2022-12-04 23:04:38 +0500

    Done

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -15,9 +15,13 @@ public class Task02Main {
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n < 1 || n > 16){
+            throw new IllegalArgumentException();
+        }
 
-        return null; // your implementation here
-
+        return IntStream.iterate(0, x -> x + 1)
+                .map(x -> x % (int) Math.pow(2, n))
+                .map(x -> x ^ (x >> 1));
     }
 
 }
