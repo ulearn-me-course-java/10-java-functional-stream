08134b01c7b7a8b1fdece1036d7aa7df3c540be8
Zhenya <zhenya951236qw@mail.ru>
2022-12-19 02:02:59 +0500

    Черкасов, ПрИ-201, Модуль 10

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,6 +1,7 @@
 package com.example.task03;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
@@ -22,6 +23,28 @@ public class Task03Main {
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
 
-        // your implementation here
+        if (stream == null || order == null || minMaxConsumer == null) {
+            throw  new NullPointerException();
+        }
+        Iterator<?extends T> iterator = stream.iterator();
+        if (!iterator.hasNext()) {
+            minMaxConsumer.accept(null, null);
+        }
+        else {
+            T min = iterator.next();
+            T max = min;
+
+            while (iterator.hasNext()) {
+                T current = iterator.next();
+                if (order.compare(min, current) > 0) {
+                    min = current;
+                }
+                if (order.compare(max, current) < 0) {
+                    max = current;
+                }
+            };
+
+            minMaxConsumer.accept(min,max);
+        }
     }
 }
---
+++ task05/src/com/example/task05/Salary.java
@@ -0,0 +1,7 @@
+package com.example.task05;
+
+public class Salary extends BaseForm<Integer> {
+    public Salary(String form, String to, int sum){
+        super(form, to, sum);
+    }
+}
