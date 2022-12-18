0a45857a229d2beac1a0168d66984631dedc42e6
Alina <wbrflf3301@outlook.com>
2022-12-18 20:25:30 +0500

    Выполнено задание 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,18 +6,23 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
-
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if(n <= 0 || n > 16) throw new IllegalArgumentException();
 
-        return null; // your implementation here
-
+        double maxValue = Math.pow(2, n);
+        int[] value = new int[] {0}, count = new int[] {0};
+        //IntSupplier
+        return IntStream.generate(() ->
+        {
+            if(count[0] == maxValue) { value[0] = 0; count[0] = 0; }
+            int res = value[0] ^ (value[0] >> 1);
+            value[0]++; count[0]++;
+            return res;
+        });
     }
-
 }
