622b36707ede6e35304ca6c6424e4106b3348b2c
polsssha <nusty11102003@gmail.com>
2023-04-21 00:29:19 +0500

    Потапова Анастасия. Задание 10

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,6 +3,7 @@ package com.example.task03;
 import java.util.Comparator;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
+import java.util.Iterator;
 
 public class Task03Main {
 
@@ -21,6 +22,28 @@ public class Task03Main {
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+        if(stream == null || order == null || minMaxConsumer == null){
+            throw new NullPointerException();
+        }
+        Iterator <? extends T> iterator = stream.iterator();
+        if(!iterator.hasNext()){
+            minMaxConsumer.accept(null, null);
+            return;
+        }
+        T min = iterator.next();
+        T max = min;
+        while (iterator.hasNext()){
+            T cur = iterator.next();
+            if(order.compare(min, cur) > 0){
+                min = cur;
+            }
+            if(order.compare(max, cur) < 0){
+                max = cur;
+            }
+        }
+        minMaxConsumer.accept(min, max);
+        return ;
 
         // your implementation here
     }
---
+++ task05/src/com/example/task05/MailMessage.java
@@ -0,0 +1,8 @@
+package com.example.task05;
+
+public class MailMessage extends BaseForm<String>{
+    public MailMessage(String form, String to, String content){
+        super(form, to, content);
+    }
+
+}
