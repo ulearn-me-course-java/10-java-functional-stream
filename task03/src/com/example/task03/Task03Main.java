263746503db85a355714f0645ccbe4b19c230b03
informant <artemiy.solodkin@mail.ru>
2023-10-06 19:55:50 +0500

    3 Done

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,8 @@
 package com.example.task03;
 
-import java.util.Comparator;
+import java.util.*;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Task03Main {
@@ -14,7 +15,14 @@ public class Task03Main {
                 (min, max) ->
                         System.out.println("min: " + min + " / max: " + max)
         );
+        Tests tests = new Tests();
 
+        tests.testEmpty();
+        tests.testExample();
+        tests.testNull();
+        tests.testBig();
+        tests.testNPE();
+        tests.testString();
     }
 
     public static <T> void findMinMax(
@@ -22,6 +30,28 @@ public class Task03Main {
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
 
-        // your implementation here
+        if (stream == null || order == null || minMaxConsumer == null){
+            throw new NullPointerException();
+        }
+
+        Iterator<? extends T> iterator = stream.iterator();
+
+        T min = null;
+        T max = null;
+        if (iterator.hasNext()){
+            min = iterator.next();
+            max = min;
+        }
+
+        while (iterator.hasNext()){
+            T currentElement = iterator.next();
+            if (order.compare(currentElement,max)>0){
+                max = currentElement;
+            }
+            if (order.compare(currentElement,min)<0){
+                min = currentElement;
+            }
+        }
+        minMaxConsumer.accept(min,max);
     }
 }
