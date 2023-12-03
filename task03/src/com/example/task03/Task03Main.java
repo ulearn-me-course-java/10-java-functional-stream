9ece1245ae389e79ba97298044bc607cd0965adc
FazylovV <vladislav.fazylov04@gmail.com>
2023-12-04 03:35:42 +0500

    task3 complete

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,9 @@
 package com.example.task03;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Task03Main {
@@ -22,6 +24,28 @@ public class Task03Main {
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
 
-        // your implementation here
+        if (stream == null || order == null || minMaxConsumer == null){
+            throw new NullPointerException();
+        }
+
+        Iterator<? extends T> iterator = stream.iterator();
+
+        if(!iterator.hasNext()) {
+            minMaxConsumer.accept(null, null);
+        } else {
+            T min = iterator.next();
+            T max = min;
+
+            while (iterator.hasNext()) {
+                T current = iterator.next();
+                if (order.compare(current, min) < 0) {
+                    min = current;
+                } else if (order.compare(current, max) > 0) {
+                    max = current;
+                }
+            }
+
+            minMaxConsumer.accept(min, max);
+        }
     }
 }
