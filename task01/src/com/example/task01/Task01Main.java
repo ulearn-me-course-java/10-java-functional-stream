ce68d23923623d8a61108b1166ae07fd7493ce4f
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2023-12-03 17:40:58 +0500

    done

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,6 +1,7 @@
 package com.example.task01;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -9,21 +10,28 @@ public class Task01Main {
 
         // TODO С корректно реализованным методом ternaryOperator должен компилироваться и успешно работать следующий код:
 
-        /*
+
         Predicate<Object> condition = Objects::isNull;
         Function<Object, Integer> ifTrue = obj -> 0;
         Function<CharSequence, Integer> ifFalse = CharSequence::length;
         Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
-        */
-
     }
 
     public static <T, U> Function<T, U> ternaryOperator(
             Predicate<? super T> condition,
             Function<? super T, ? extends U> ifTrue,
             Function<? super T, ? extends U> ifFalse) {
+        if(condition == null || ifTrue == null || ifFalse == null) {
+            throw new NullPointerException();
+        }
 
-        return null; // your implementation here
-
+        return x -> {
+            if(condition.test(x)){
+                return ifTrue.apply(x);
+            }
+            else {
+                return ifFalse.apply(x);
+            }
+        };
     }
 }
--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -9,8 +9,6 @@ public class Task05Main {
 
     public static void main(String[] args) {
 
-        /*
-
         // Random variables
         String randomFrom = "..."; // Некоторая случайная строка. Можете выбрать ее самостоятельно.
         String randomTo = "...";  // Некоторая случайная строка. Можете выбрать ее самостоятельно.
@@ -26,6 +24,7 @@ public class Task05Main {
         assert firstMessage.getFrom().equals("Robert Howard") : "Wrong firstMessage from address";
         assert firstMessage.getTo().equals("H.P. Lovecraft") : "Wrong firstMessage to address";
         assert firstMessage.getContent().endsWith("Howard!") : "Wrong firstMessage content ending";
+
 
         MailMessage secondMessage = new MailMessage(
                 "Jonathan Nolan",
@@ -53,19 +52,18 @@ public class Task05Main {
         //   где по получателю можно получить список сообщений, которые были ему отправлены
         Map<String, List<String>> mailBox = mailService.getMailBox();
 
+
         assert mailBox.get("H.P. Lovecraft").equals(
                 Arrays.asList(
                         "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!"
                 )
         ) : "wrong mailService mailbox content (1)";
-
         assert mailBox.get("Christopher Nolan").equals(
                 Arrays.asList(
                         "Брат, почему все так хвалят только тебя, когда практически все сценарии написал я. Так не честно!",
                         "Я так и не понял Интерстеллар."
                 )
         ) : "wrong mailService mailbox content (2)";
-
         assert mailBox.get(randomTo).equals(Collections.<String>emptyList()) : "wrong mailService mailbox content (3)";
 
 
@@ -86,10 +84,5 @@ public class Task05Main {
         assert salaries.get(salary1.getTo()).equals(Arrays.asList(1)) : "wrong salaries mailbox content (1)";
         assert salaries.get(salary2.getTo()).equals(Arrays.asList(Integer.MAX_VALUE)) : "wrong salaries mailbox content (2)";
         assert salaries.get(randomTo).equals(Arrays.asList(randomSalary)) : "wrong salaries mailbox content (3)";
-
-
-        */
-
     }
-
 }
