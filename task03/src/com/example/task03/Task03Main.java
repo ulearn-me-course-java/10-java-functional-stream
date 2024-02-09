7d0ebde4c9a8dd66ba9a8267566580759ba2d2c7
MabusTFC <113986000+MabusTFC@users.noreply.github.com>
2024-02-09 21:08:43 +0500

    10.Смирнов ИА. ПрИ-201

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,26 +1,43 @@
 package com.example.task03;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
 public class Task03Main {
-
     public static void main(String[] args) {
-
         findMinMax(
                 Stream.of(2, 9, 5, 4, 8, 1, 3),
                 Integer::compareTo,
                 (min, max) ->
                         System.out.println("min: " + min + " / max: " + max)
         );
-
     }
-
     public static <T> void findMinMax(
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
+        if (stream == null || order == null || minMaxConsumer == null){
+            throw new NullPointerException();
+        }
+        Iterator<? extends T> iterator = stream.iterator();
+        if (!iterator.hasNext()){
+            minMaxConsumer.accept(null, null);
+            return;
+        }
+        T min = iterator.next();
+        T max = min;
+
+        while(iterator.hasNext()){
+            T curr = iterator.next();
+            if (order.compare(curr, min) < 0) {
+                min = curr;
+            } else if (order.compare(curr, max) > 0) {
+                max = curr;
+            }
+        }
+        minMaxConsumer.accept(min, max);
 
         // your implementation here
     }
