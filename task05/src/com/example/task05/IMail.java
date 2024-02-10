412287d2a6496e08d9cac114e093b32a38b7bfb3
Anatoliy <magic-t@bk.ru>
2024-02-10 23:14:21 +0500

    without 5s

---
+++ task05/src/com/example/task05/IMail.java
@@ -0,0 +1,25 @@
+package com.example.task05;
+
+public class IMail<T> {
+    private String from;
+    private String to;
+    private T content;
+
+    public IMail(String from, String to, T content) {
+        this.from = from;
+        this.to = to;
+        this.content = content;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public T getContent() {
+        return content;
+    }
+}
